d73cf7db67468148beb8a41c403bbb5ab0228f87
semenov-ol <semenov.os16@gmail.com>
2020-09-30 18:58:44 +0300

    refactor: Change styles for header

--- components/header/header.styles.ts
+++ components/header/header.styles.ts
@@ -6,9 +6,9 @@ const HeaderContainer = styled.div`
   align-items: center;
 
   width: 100%;
-  height: 60px;
+  height: 70px;
 
-  background-color: #95d7ff;
+  background-color: #91b4ff;
 `;
 
 const NavContainer = styled.div`
@@ -17,6 +17,21 @@ const NavContainer = styled.div`
   justify-content: space-around;
 
   width: 100%;
+  a {
+    display: block;
+    background: #1a81db;
+    padding: 15px;
+    height: 50px;
+    color: white;
+    border-radius: 8px;
+    :hover {
+      background: #1e96ff;
+      text-transform: none;
+    }
+    :active {
+      background: #64a8ff;
+    }
+  }
 `;
 
 const ButtonContainer = styled.div`
